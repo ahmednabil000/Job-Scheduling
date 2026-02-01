9970234f5f1a6d383330656ee7fa328a581cfe5e
Ahmed Nabil <80886379+ahmednabil000@users.noreply.github.com>
2026-02-01 06:11:42 +0200

    some changes

--- src/jobs/factories/job-validator.factory.ts
+++ src/jobs/factories/job-validator.factory.ts
@@ -12,7 +12,7 @@ export class JobValidatorFactory {
       case 'sms-sender':
         return smsSenderJobValidator;
       default:
-        return null; // Or throw error, but service handles "unknown" usually
+        return null; 
     }
   }
 }
